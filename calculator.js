b7e46125c75e274d4537550d699b6d50b758b516
Subsdip <prayforsubc@outlook.com>
2022-07-22 10:42:58 +0530

    Calculator

---
+++ calculator.js
@@ -0,0 +1,28 @@
+class calculator{
+  constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear(){
+    this.previousOperandTextElement = '';
+    this.currentOperandTextElement = '';
+
+  }
+
+  delete(){
+    this.currentOperandTextElement = 
+  }
+}
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
